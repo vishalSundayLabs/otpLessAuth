4ceab28e744e9762fbeb56d1e1c432b509ff5ee7
Apple <apple@Apples-MacBook-Pro.local>
2023-04-03 11:54:24 +0530

    test otpless

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // Define the 'otpless' function
+    window.otpless = (otplessUser) => {
+     // Retrieve the user's details after successful login
+     const waName = otplessUser.waName;
+     const waNumber = otplessUser.waNumber;
+         
+     // Handle the signup/signin process
+     // ...
+    };
+   }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>ok</p>
     </div>
   );
 }

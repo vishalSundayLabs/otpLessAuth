1906368a93e86d70519fe513e353976771a97fc9
Apple <apple@Apples-MacBook-Pro.local>
2023-04-03 13:14:43 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import Signin from './signIn';
+import Success from './Success';
 
 function App() {
-  useEffect(() => {
-    // Define the 'otpless' function
-    window.otpless = (otplessUser) => {
-     // Retrieve the user's details after successful login
-     const waName = otplessUser.waName;
-     const waNumber = otplessUser.waNumber;
-         
-     console.log('otpless console : ',otplessUser)
-     console.log(waName)
-     // Handle the signup/signin process
-     // ...
-    };
-   }, []);
   return (
-    <div className="App">
-      <div id="otpless"></div>
-    </div>
+    <>
+       <NavLink>
+        <Route path='/' Component={Signin}></Route>
+        <Route path='/success' Component={Success}></Route>
+    </NavLink>
+    </>
+
   );
 }
 
---
+++ src/signIn.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+const Signin = () => {
+    let Nav = useNavigate()
+    useEffect(() => {
+        // Define the 'otpless' function
+        window.otpless = (otplessUser) => {
+         // Retrieve the user's details after successful login
+         const waName = otplessUser.waName;
+         const waNumber = otplessUser.waNumber;
+             
+         console.log('otpless console : ',otplessUser)
+         console.log(waName)
+        window.location.href = Nav('/success')
+         // Handle the signup/signin process
+         // ...
+        };
+       }, []);
+  return (
+    <div>Signin</div>
+  )
+}
+
+export default Signin
